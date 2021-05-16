dda009e4d5d03e7ba8eb6eb0c19acf0e872fa278
Dainius Serplis <dserplis@vmware.com>
2021-05-16 22:33:49 +0300

    Fix LDAP tests
    Signed-off-by: Dainius Serplis <dserplis@vmware.com>

---
+++ govcd/nsxt_application_profile_test.go
@@ -0,0 +1,30 @@
+// +build network nsxt functional openapi ALL
+
+package govcd
+
+import (
+	"fmt"
+
+	"github.com/vmware/go-vcloud-director/v2/types/v56"
+	. "gopkg.in/check.v1"
+)
+
+func (vcd *TestVCD) Test_NsxtApplicationPortProfile(check *C) {
+	skipNoNsxtConfiguration(vcd, check)
+	skipOpenApiEndpointTest(vcd, check, types.OpenApiPathVersion1_0_0+types.OpenApiEndpointAppPortProfiles)
+
+	org, err := vcd.client.GetOrgByName(vcd.config.VCD.Org)
+	check.Assert(err, IsNil)
+
+	nsxtVdc, err := org.GetVDCByName(vcd.config.VCD.Nsxt.Vdc, false)
+	check.Assert(err, IsNil)
+
+	edge, err := nsxtVdc.GetNsxtEdgeGatewayByName(vcd.config.VCD.Nsxt.EdgeGateway)
+	check.Assert(err, IsNil)
+
+	appProf, err := edge.GetAllNsxtAppPortProfiles(nil, "SYSTEM")
+	check.Assert(err, IsNil)
+	check.Assert(len(appProf) > 0, Equals, true)
+	fmt.Println(len(appProf))
+
+}
